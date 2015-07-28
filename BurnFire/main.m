5cc4c3b1ad12807c406d7f28ad1bd24944947713
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-07-28 23:13:37 +0700

    Initial Commit

---
+++ BurnFire/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BurnFire
+//
+//  Created by admin on 7/28/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
